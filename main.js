d8e9e913653f68831718d00d7208223783b813cb
Wwashy <52789151+Wwashy@users.noreply.github.com>
2020-04-11 23:48:05 +0300

    update

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+var bodyparser = require('body-parser');
+
+
+//mysql://b306d28a03c4b7:3ecbb6ab@us-cdbr-iron-east-01.cleardb.net/heroku_76b072eab9306cc?reconnect=true
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+var connection = mysql.createPool({
+    connectionLimit: 100,
+    host:'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b306d28a03c4b7',
+    password:'3ecbb6ab',
+    database: 'heroku_76b072eab9306cc',
+    multipleStatements: true
+});
+
+connection.getConnection((err)=>{
+if(err) throw err;
+console.log('connected');
+});
 
 app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/message',(req,res)=>{
+    var sql = "INSERT INTO messages (sender_name,sender_message) VALUES('"+req.body.sender+"','"+req.body.message+"')";
+    connection.query(sql,(err)=>{
+        if(err) throw err;
+        console.log('sent!');
+    });
     res.sendFile(__dirname + '/index.html');
 });
  app.listen(app.get('port'),(err)=>{if(err) throw err; console.log('listenning to port 3000',app.get('port'))});
