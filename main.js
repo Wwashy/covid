70a6b8d0cacbc133c3b0273fc0a3e12d234e4556
Wwashy <52789151+Wwashy@users.noreply.github.com>
2020-04-11 16:30:19 +0300

    proces env update

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
- app.listen(3000,(err)=>{if(err) throw err; console.log('listenning to port 3000')});
+ app.listen(app.get('port'),(err)=>{if(err) throw err; console.log('listenning to port 3000',app.get('port'))});
