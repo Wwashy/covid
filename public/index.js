e597a0afc4212b325677c69a7bef50fc238921bc
Wwashy <52789151+Wwashy@users.noreply.github.com>
2020-04-13 22:35:41 +0300

    added menu

--- public/index.js
+++ public/index.js
@@ -3,7 +3,16 @@ function open (){
     document.getElementById("navi").style.display = "block";
 return;
 }
+
 function close () {
     document.getElementById("navi").style.display= "none";  
     return;      
+}
+var close_btn = document.getElementById("close");
+close_btn.onclick = closeMenu();
+function showMenu() {
+    document.getElementById("navi").style.top = "0px";
+}
+function closeMenu() {
+        document.getElementById("navi").style.top = "-1000px";
 }
