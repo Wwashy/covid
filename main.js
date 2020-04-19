012c5600ba1618e51c8615373079d7765f20c551
Wwashy <52789151+Wwashy@users.noreply.github.com>
2020-04-19 16:17:32 +0300

    global and kenya case

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
-var bodyparser = require('body-parser');
+let express = require('express');
+let app = express();
+let mysql = require('mysql');
+let bodyparser = require('body-parser');
 
 
 //mysql://b306d28a03c4b7:3ecbb6ab@us-cdbr-iron-east-01.cleardb.net/heroku_76b072eab9306cc?reconnect=true
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(bodyparser.urlencoded({extended:true}));
 
 
-var connection = mysql.createPool({
+let connection = mysql.createPool({
     connectionLimit: 100,
     host:'us-cdbr-iron-east-01.cleardb.net',
     user: 'b306d28a03c4b7',
@@ -29,11 +29,59 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 app.post('/message',(req,res)=>{
-    var sql = "INSERT INTO messages (sender_name,sender_message) VALUES('"+req.body.sender+"','"+req.body.message+"')";
+    let sql = "INSERT INTO messages (sender_name,sender_message) VALUES('"+req.body.sender+"','"+req.body.message+"')";
     connection.query(sql,(err)=>{
         if(err) throw err;
         console.log('sent!');
     });
     res.sendFile(__dirname + '/index.html');
+});
+//global
+app.post('/data',(req,res)=>{
+    let sql = "SELECT * FROM global_stat;";
+    connection.query(sql,(err,result)=>{
+        if (err) throw err;
+        let data ={
+            infection:result[0].stat_infection,
+            recovered: result[0].stat_recovered,
+            death:result[0].stat_death
+        }
+        res.send(data);
+        console.log(data);
+    });
+});
+//global
+app.post('/dataKE',(req,res)=>{
+    let sql = "SELECT * FROM kenya_stat;";
+    connection.query(sql,(err,result)=>{
+        if (err) throw err;
+        let data ={
+            infection: result[0].stat_infection,
+            recovered: result[0].stat_recovered,
+            death: result[0].stat_death
+        }
+        res.send(data);
+        console.log(result);
+    });
+});
+// updates the content
+app.post('/getBlog',(req,res)=>{
+
+    let sql = "\
+    SELECT * FROM about_content;\
+    SELECT * FROM prevention_content;\
+    SELECT * FROM cure_content;\
+    SELECT * FROM contact_content;\
+    ";
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        let data ={
+            about:result.about_content.content,
+            prevention:result.prevention_content.content,
+            spread:result.spread_content.content,
+            contact: result.contact_content.content
+        }
+        res.send(data);
+    });
 });
  app.listen(app.get('port'),(err)=>{if(err) throw err; console.log('listenning to port 3000',app.get('port'))});
---
+++ public/update.js
@@ -0,0 +1,31 @@
+$(document).ready(()=>{
+    let devDiv = document.createElement("div");
+    devDiv.setAttribute("class","development");
+    $()
+    //updates the statistics
+    $.ajax({
+        url: '/data',
+        type: 'POST',
+        dataType: 'json',
+        success:(result)=>{
+            console.log(result);
+            $('#infection').html(result.infection);
+            $('#recovered').html(result.recovered);
+            $('#death').html(result.death);
+        }
+    });
+
+    //kenya
+    $.ajax({
+        url: '/dataKE',
+        type: 'POST',
+        dataType: 'json',
+        success:(result)=>{
+            console.log(result);
+            $('#infection_ke').html(result.infection);
+            $('#recovered_ke').html(result.recovered);
+            $('#death_ke').html(result.death);
+        }
+    })
+
+});
